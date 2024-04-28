bea3ec0c7e91e7d2d5209f8474b139d38452c251
feryanuar24 <feryanuar24@gmail.com>
2024-04-28 20:31:19 +0700

    feat:animation

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -1,141 +1,164 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 
 const Works = () => {
   return (
     <section id="works" className="works">
-      <h2>Explore some of my work</h2>
-      <div className="gallery">
-        <div>
+      <Fade direction="left">
+        <h2>Explore some of my work</h2>
+        <div className="gallery">
           <div>
-            <img src="./img/calculator.png" alt="Calculator App" width={200} />
+            <div>
+              <img
+                src="./img/calculator.png"
+                alt="Calculator App"
+                width={200}
+              />
+            </div>
+            <p>Calculator App</p>
           </div>
-          <p>Calculator App</p>
-        </div>
-        <div>
           <div>
-            <img src="./img/car.png" alt="Car Rental App" width={200} />
+            <div>
+              <img src="./img/car.png" alt="Car Rental App" width={200} />
+            </div>
+            <p>Car Rental App</p>
           </div>
-          <p>Car Rental App</p>
-        </div>
-        <div>
           <div>
-            <img src="./img/comic.png" alt="Comic Management App" width={200} />
+            <div>
+              <img
+                src="./img/comic.png"
+                alt="Comic Management App"
+                width={200}
+              />
+            </div>
+            <p>Comic Management App</p>
           </div>
-          <p>Comic Management App</p>
-        </div>
-        <div>
           <div>
-            <img
-              src="./img/flight.png"
-              alt="Flight Ticket Booking App"
-              width={200}
-            />
+            <div>
+              <img
+                src="./img/flight.png"
+                alt="Flight Ticket Booking App"
+                width={200}
+              />
+            </div>
+            <p>Flight Ticket Booking App</p>
           </div>
-          <p>Flight Ticket Booking App</p>
-        </div>
-        <div>
           <div>
-            <img src="./img/gallery.png" alt="Photo Galery App" width={200} />
+            <div>
+              <img src="./img/gallery.png" alt="Photo Galery App" width={200} />
+            </div>
+            <p>Photo Galery App</p>
           </div>
-          <p>Photo Galery App</p>
-        </div>
-        <div>
           <div>
-            <img
-              src="./img/library.png"
-              alt="Library Management App"
-              width={200}
-            />
+            <div>
+              <img
+                src="./img/library.png"
+                alt="Library Management App"
+                width={200}
+              />
+            </div>
+            <p>Library Management App</p>
           </div>
-          <p>Library Management App</p>
-        </div>
-        <div>
           <div>
-            <img src="./img/lpmunsika.png" alt="News Portal App" width={200} />
+            <div>
+              <img
+                src="./img/lpmunsika.png"
+                alt="News Portal App"
+                width={200}
+              />
+            </div>
+            <p>News Portal App</p>
           </div>
-          <p>News Portal App</p>
-        </div>
-        <div>
           <div>
-            <img src="./img/movie.png" alt="Movie Search App" width={200} />
+            <div>
+              <img src="./img/movie.png" alt="Movie Search App" width={200} />
+            </div>
+            <p>Movie Search App</p>
           </div>
-          <p>Movie Search App</p>
-        </div>
-        <div>
           <div>
-            <img
-              src="./img/parking.png"
-              alt="Parking Management App"
-              width={200}
-            />
+            <div>
+              <img
+                src="./img/parking.png"
+                alt="Parking Management App"
+                width={200}
+              />
+            </div>
+            <p>Parking Management App</p>
           </div>
-          <p>Parking Management App</p>
-        </div>
-        <div>
           <div>
-            <img src="./img/pokedex.png" alt="Pokemon Index App" width={200} />
+            <div>
+              <img
+                src="./img/pokedex.png"
+                alt="Pokemon Index App"
+                width={200}
+              />
+            </div>
+            <p>Pokemon Index App</p>
           </div>
-          <p>Pokemon Index App</p>
-        </div>
-        <div>
           <div>
-            <img
-              src="./img/spk-pembelian.png"
-              alt="SPK Pembelian Mobil"
-              width={200}
-            />
+            <div>
+              <img
+                src="./img/spk-pembelian.png"
+                alt="SPK Pembelian Mobil"
+                width={200}
+              />
+            </div>
+            <p>SPK Pembelian Mobil</p>
           </div>
-          <p>SPK Pembelian Mobil</p>
-        </div>
-        <div>
           <div>
-            <img
-              src="./img/spk-sungai.png"
-              alt="SPK Kualitas Air"
-              width={200}
-            />
+            <div>
+              <img
+                src="./img/spk-sungai.png"
+                alt="SPK Kualitas Air"
+                width={200}
+              />
+            </div>
+            <p>SPK Kualitas Air</p>
           </div>
-          <p>SPK Kualitas Air</p>
-        </div>
-        <div>
           <div>
-            <img
-              src="./img/star-wars.png"
-              alt="Star Wars Trivia App"
-              width={200}
-            />
+            <div>
+              <img
+                src="./img/star-wars.png"
+                alt="Star Wars Trivia App"
+                width={200}
+              />
+            </div>
+            <p>Star Wars Trivia App</p>
           </div>
-          <p>Star Wars Trivia App</p>
-        </div>
-        <div>
           <div>
-            <img src="./img/suwit.png" alt="Suwit App" width={200} />
+            <div>
+              <img src="./img/suwit.png" alt="Suwit App" width={200} />
+            </div>
+            <p>Suwit App</p>
           </div>
-          <p>Suwit App</p>
-        </div>
-        <div>
           <div>
-            <img src="./img/todo.png" alt="Todo List App" width={200} />
+            <div>
+              <img src="./img/todo.png" alt="Todo List App" width={200} />
+            </div>
+            <p>Todo List App</p>
           </div>
-          <p>Todo List App</p>
-        </div>
-        <div>
           <div>
-            <img
-              src="./img/werehouse.png"
-              alt="Werehouse Management App"
-              width={200}
-            />
+            <div>
+              <img
+                src="./img/werehouse.png"
+                alt="Werehouse Management App"
+                width={200}
+              />
+            </div>
+            <p>Werehouse Management App</p>
           </div>
-          <p>Werehouse Management App</p>
-        </div>
-        <div>
           <div>
-            <img src="./img/yugioh.png" alt="Yu-Gi-Oh Trivia App" width={200} />
+            <div>
+              <img
+                src="./img/yugioh.png"
+                alt="Yu-Gi-Oh Trivia App"
+                width={200}
+              />
+            </div>
+            <p>Yu-Gi-Oh Trivia App</p>
           </div>
-          <p>Yu-Gi-Oh Trivia App</p>
         </div>
-      </div>
+      </Fade>
     </section>
   );
 };
